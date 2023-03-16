459c886f71e66a871a00b00c61baa513301431f0
U-BSD\412237 <412237@SHS-DGCSCS2.beaverton.k12.or.us>
2023-03-16 14:22:32 -0700

    on it more

--- tree.cpp
+++ tree.cpp
@@ -8,7 +8,33 @@ void print(branch* head){
 }
 
 int add(branch* current, char input){
+  branch* b = new branch;
+  if(current->data == 'H'){
+    cout << "one" << endl;
+    set(current, input);
+    return 1;
+  }
   
+  if(current->right == NULL){
+    cout << "two" << endl;
+    current->right = b;
+    set(b, input);
+    return 1;
+  }
+  if(!isdigit(current->right->data)){
+    cout << "three" << endl;
+    add(current->right, input);
+  }
+  if(current->left == NULL){
+    cout << "four" << endl;
+    current->left = b;
+    set(b, input);
+    return 1;
+  }
+  if(!isdigit(current->left->data)){
+    cout << "five" << endl;
+    add(current->left, input);  
+  }
 }
 
 void set(branch* b, char input){
